0cce7bd025a371c1e35f7a0f4a494516a8f313a4
lidong27 <lidongrabbit@163.com>
2020-05-30 11:34:06 +0800

    init

---
+++ product-view-service-feign/src/main/java/cn/how2j/springcloud/web/ProductController.java
@@ -0,0 +1,28 @@
+package cn.how2j.springcloud.web;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.how2j.springcloud.pojo.Product;
+import cn.how2j.springcloud.service.ProductService;
+ 
+@Controller
+public class ProductController {
+ 
+	@Autowired ProductService productService;
+
+    @Value("${version}")
+    String version;
+
+    @RequestMapping("/products")
+    public Object products(Model m) {
+    	List<Product> ps = productService.listProducts();
+    	m.addAttribute("ps", ps);
+        m.addAttribute("version", version);
+        return "products";
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,2 @@
+启动链路追踪服务器:
+java -jar zipkin-server-2.10.1-exec.jar
